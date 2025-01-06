6173bd7f44c1321e1dbdc2dd3396b4cf5741e134
Alwin Joseph <alwinjoseph.chris@gmail.com>
2025-01-06 22:41:06 +0000

    WIP Login POM and Step definitions

--- src/main/java/org/assessment/pages/LoginPage.java
+++ src/main/java/org/assessment/pages/LoginPage.java
@@ -1,12 +1,20 @@
 package org.assessment.pages;
 
+import org.assessment.framewokFunctions.BaseTest;
+import org.assessment.framewokFunctions.exceptionHandler.UserException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 
-public class LoginPage {
+import static org.testng.Assert.*;
 
+public class LoginPage implements BaseTest {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginPage.class);
     WebDriver driver;
     @FindBy(name = "username")
     WebElement usernameField;
@@ -14,15 +22,74 @@ public class LoginPage {
     WebElement passwordField;
     @FindBy(xpath = "//button[normalize-space()='Login']")
     WebElement loginButton;
+    @FindBy(xpath = "//p[normalize-space()='Forgot your password?']")
+    WebElement forgetPasswordButton;
+    @FindBy(xpath = "//p[text()='Invalid credentials']")
+    WebElement invalidError;
+    @FindBy(xpath = "//label[text()='Username']/../..//span[text()='Required']")
+    WebElement userNameRequiredError;
+    @FindBy(xpath = "//label[text()='Password']/../..//span[text()='Required']")
+    WebElement passwordRequiredError;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
-    public void login(String username, String password) {
-        usernameField.sendKeys(username);
-        passwordField.sendKeys(password);
-        loginButton.click();
+    public void verifyLoginPageDisplayed() throws UserException {
+        screenShotUtil.captureScreenshot("LoginPage");
+        assertTrue(usernameField.isDisplayed());
+        assertTrue(passwordField.isDisplayed());
+        assertTrue(loginButton.isDisplayed());
     }
+
+    public void enterDetails(String field, String value) throws UserException {
+        switch (field.toLowerCase().trim()) {
+            case "username" :
+                inputUtil.enterText(usernameField, value);
+                break;
+            case "password" :
+                inputUtil.enterText(passwordField, value);
+                break;
+            default:
+                throw new UserException("Invalid field to enter data in Login page");
+        }
+    }
+
+    public void performAction(String action) throws UserException {
+        switch (action.toLowerCase().trim()) {
+            case "login" :
+                inputUtil.click(loginButton);
+                break;
+            case "forgot your password?" :
+                inputUtil.click(forgetPasswordButton);
+                break;
+            default:
+                throw new UserException("Invalid action to perform in Login page");
+        }
+    }
+
+    public void loginWithCredentials(String username, String password) throws UserException {
+        enterDetails("username", username);
+        enterDetails("password", password);
+
+        screenShotUtil.captureScreenshot("LoginPage");
+
+        performAction("login");
+    }
+
+    public void verifyErrorMessage(String errorMessage) throws UserException {
+        assertTrue(invalidError.isDisplayed());
+        assertEquals(invalidError.getText(), errorMessage);
+        screenShotUtil.captureScreenshot("LoginError");
+    }
+
+    public void verifyErrorForBothMandatoryFields() {
+        assertTrue(userNameRequiredError.isDisplayed());
+        assertTrue(passwordRequiredError.isDisplayed());
+    }
+
+
+
+
 }
--- src/test/java/org/assessment/stepDefinitions/LoginPageStepDef.java
+++ src/test/java/org/assessment/stepDefinitions/LoginPageStepDef.java
@@ -1,38 +1,74 @@
 package org.assessment.stepDefinitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.bs.A;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.assessment.framewokFunctions.utility.WebDriverUtil;
+import org.assessment.framewokFunctions.exceptionHandler.UserException;
 import org.assessment.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginPageStepDef {
 
+    private static final Logger log = LoggerFactory.getLogger(LoginPageStepDef.class);
     WebDriver driver;
     LoginPage loginPage;
 
+    public LoginPageStepDef() throws UserException {
+    }
+
     @Given("I am on the login page")
-    public void navigateToLoginPage() {
-        driver = new ChromeDriver();
+    public void navigateToLoginPage() throws UserException {
+        driver= WebDriverUtil.getDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         loginPage = new LoginPage(driver);
+        loginPage.verifyLoginPageDisplayed();
     }
 
-    @When("I enter valid username and password")
-    public void enterValidCredentials() {
-        loginPage.login("Admin", "admin123");
+    @When("I enter valid {string} and {string}")
+    public void enterValidCredentials(String username, String password) throws UserException {
+        loginPage.loginWithCredentials(username, password);
     }
 
-    @Then("I should be redirected to the dashboard")
-    public void verifyDashboard() {
-        Assert.assertEquals(driver.getTitle(), "Dashboard");
+    @When("I enter invalid {string} and {string}")
+    public void enterInValidCredentials(String username, String password) throws UserException {
+        loginPage.loginWithCredentials(username, password);
     }
 
-    @After
-    public void tearDown() {
-        driver.quit();
+    @Then("I should see an error message {string}")
+    public void verifyErrorMessage(String errorMessage) throws UserException {
+        loginPage.verifyErrorMessage(errorMessage);
+    }
+
+    @When("I click on the {string} link")
+    public void performAction(String action) throws UserException {
+        loginPage.performAction(action);
+    }
+
+    @And("close the browser")
+    public void closeBrowser() {
+        WebDriverUtil.closeDriver();
+    }
+
+    @Then("I should see error message for all mandatory fields")
+    public void verifyMandatoryFields() {
+        loginPage.verifyErrorForBothMandatoryFields();
+    }
+
+    @Then("I should be redirected to the login page")
+    public void iShouldBeRedirectedToTheLoginPage() throws UserException {
+        loginPage.verifyLoginPageDisplayed();
+    }
+
+    @Given("I navigate to the dashboard page without logging in")
+    public void iNavigateToTheDashboardPageWithoutLoggingIn() throws UserException {
+        driver= WebDriverUtil.getDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+        loginPage = new LoginPage(driver);
     }
 }
